f5ceed2e539f394017b29fb370b0b73eac8f0efc
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2024-04-02 23:22:21 +0530

    Add files via upload

--- Basics Done/Binarysearch.py
+++ Basics Done/Binarysearch.py
@@ -12,4 +12,12 @@ def binearSeach(ordered_list,key):
             first=mid+1
     return False
 
-print(binearSeach([2,5,10,12,15,17,20],15))
+def BinarySearch(ordered_list,key):
+    if len(ordered_list)==0: return False
+    else:
+        mid = len(ordered_list)//2
+        if ordered_list[mid]==key:return True
+        elif ordered_list[mid]>key:return BinarySearch(ordered_list[:mid],key)
+        else: return BinarySearch(ordered_list[mid+1:],key)
+
+print(BinarySearch([2,5,10,12,15,17,20],15))
